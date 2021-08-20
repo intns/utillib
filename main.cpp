b104d0865fda4e302ed4b2b93b8ee70b03d5b2e5
intns <84647527+intns@users.noreply.github.com>
2021-08-20 16:03:09 +0100

    add float reading to fstream

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <util/fstream_reader.hpp>
 #include <util/misc.hpp>
 #include <util/vector_reader.hpp>
@@ -8,17 +8,18 @@ int main(int argc, char** argv)
 {
     // CHECKING DUMP FUNCTIONS
     std::ofstream dump_out("out.txt", std::ios_base::binary);
-    dump_out.write("test test dump to hex test", 26);
+    dump_out.write("D¾@�", 4);
     dump_out.close();
     util::DumpFileToCppHeader("out.txt");
 
     // CHECKING READING FUNCTIONS
-    util::vector_reader vector_reader({ 80, 16, 2, 3, 4, 5, 6 }, 0, util::vector_reader::Endianness::Big);
+    util::vector_reader vector_reader({ 0x10, 0x01 }, 0, util::vector_reader::Endianness::Big);
     std::cout << vector_reader.readS16() << std::endl;
 
     std::ifstream fstream_reader_file("out.txt", std::ios_base::binary);
     util::fstream_reader fstream_reader(fstream_reader_file);
-    std::cout << (u32)fstream_reader.readU8() << std::endl;
+    fstream_reader.endianness() = util::fstream_reader::Endianness::Big;
+    std::cout << fstream_reader.readF32() << std::endl;
     fstream_reader_file.close();
 
     // CHECKING ANAGRAM FUNCTIONS
