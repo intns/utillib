3f9c9010884bdf9b71acb5b362f020537bed7bc1
intns <84647527+intns@users.noreply.github.com>
2021-08-25 21:00:58 +0100

    Update types.hpp

--- types.hpp
+++ types.hpp
@@ -1,47 +1,35 @@
-#ifndef _TYPES_HPP
-#define _TYPES_HPP
-
 #pragma once
-#include <stdbool.h>
 #include <stdint.h>
+#include <stdbool.h>
 
-#ifdef __CWCC__
-typedef signed char s8;
-typedef signed short s16;
-typedef signed long s32;
-typedef signed long long s64;
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned long u32;
-typedef unsigned long long u64;
-#elif defined(__GNUC__) // GCC
-typedef signed char s8;
-typedef signed short s16;
-typedef signed int s32;
-typedef signed long long s64;
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned int u32;
-typedef unsigned long long u64;
-#elif defined(_MSC_VER) // Win32
-typedef __int8 s8;
-typedef __int16 s16;
-typedef __int32 s32;
-typedef __int64 s64;
-typedef unsigned __int8 u8;
-typedef unsigned __int16 u16;
-typedef unsigned __int32 u32;
-typedef unsigned __int64 u64;
-#endif
-typedef float f32;
-typedef double f64;
+typedef signed char         s8;
+typedef signed short        s16;
+typedef signed long         s32;
+typedef signed long long    s64;
+typedef unsigned char       u8;
+typedef unsigned short      u16;
+typedef unsigned long       u32;
+typedef unsigned long long  u64;
+
+typedef volatile u8         vu8;
+typedef volatile u16        vu16;
+typedef volatile u32        vu32;
+typedef volatile u64        vu64;
+typedef volatile s8         vs8;
+typedef volatile s16        vs16;
+typedef volatile s32        vs32;
+typedef volatile s64        vs64;
+
+typedef float               f32;
+typedef double              f64;
+typedef volatile f32        vf32;
+typedef volatile f64        vf64;
 
 #ifdef __cplusplus
-static_assert(sizeof(s8) == 1 && sizeof(u8) == 1, "!");
-static_assert(sizeof(s16) == 2 && sizeof(u16) == 2, "!");
-static_assert(sizeof(s32) == 4 && sizeof(u32) == 4, "!");
-static_assert(sizeof(f32) == 4, "!");
-static_assert(sizeof(f64) == 8, "!");
-#endif
-
+static_assert(sizeof(s8) == 1 && sizeof(u8) == 1,   "8 bit number isn't 1 byte big");
+static_assert(sizeof(s16) == 2 && sizeof(u16) == 2, "16 bit number isn't 2 bytes big");
+static_assert(sizeof(s32) == 4 && sizeof(u32) == 4, "32 bit number isn't 4 bytes big");
+static_assert(sizeof(s64) == 8 && sizeof(u64) == 8, "64 bit number isn't 8 bytes big");
+static_assert(sizeof(f32) == 4, "32 bit floating point number isn't 4 bytes big");
+static_assert(sizeof(f64) == 8, "64 bit floating point number isn't 8 bytes big");
 #endif
